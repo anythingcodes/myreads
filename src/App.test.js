e68cc43548c3cf47035ebfcf6f8d7338da1ec401
Liz Shaw <liz.h.shaw@gmail.com>
2021-03-02 15:43:45 -0500

    Update placeholder App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App'
 
-/** 
- This course is not designed to teach Test Driven Development. 
- Feel free to use this file to test your application, but it 
+/**
+ This course is not designed to teach Test Driven Development.
+ Feel free to use this file to test your application, but it
  is not required.
 **/
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
+  ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, div);
+});
 
 
